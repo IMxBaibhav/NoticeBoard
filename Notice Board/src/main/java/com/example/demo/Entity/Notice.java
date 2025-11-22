4c92a098bbea53c9eaa2cbd8c0d8775f892e984c
lexendx <tiwaribaibhav2004@gmail.com>
2025-11-22 15:43:54 +0530

    first commit

---
+++ Notice Board/src/main/java/com/example/demo/Entity/Notice.java
@@ -0,0 +1,13 @@
+package com.example.demo.Entity;
+import jakarta.persistence.*;
+import lombok.*;
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+
+
+public class Notice {
+    
+}
