5e20234d976c839b6fb9507459bc317c3f381b77
lexendx <tiwaribaibhav2004@gmail.com>
2025-11-22 17:13:33 +0530

    minor upgrade

---
+++ Notice Board/src/main/java/com/example/demo/controller/NoticeController.java
@@ -0,0 +1,14 @@
+package com.example.demo.controller;
+import com.example.demo.service.NoticeService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/notices")
+@RequiredArgsConstructor
+
+public class NoticeController {
+    
+}
--- Notice Board/src/main/java/com/example/demo/service/NoticeServiceImpl.java
+++ Notice Board/src/main/java/com/example/demo/service/NoticeServiceImpl.java
@@ -5,7 +5,27 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+@Service
+@RequiredargsConstructor
 
-public class NoticeServiceImpl {
-    
+public class NoticeServiceImpl implements NoticeService {
+    private final NoticeRepository repository;
+
+    @Override 
+    public createNotice(Notice notice){
+        return repository.save(notice);
+    }
+@Override 
+public List<Notice> getAllNotices(){
+    return repository.findAll();
+
+}
+@Override
+public Notice getNoticeById(Long Id){
+    return repository.findBy(Id).orElse(Null);
+}
+@Override 
+public void deleteNotice(Long Id){
+    repository.deleteById(Id);
+}
 }
